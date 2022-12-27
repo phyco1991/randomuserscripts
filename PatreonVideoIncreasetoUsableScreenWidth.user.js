83775d7c894cd2dce2092a30274553f7c9d2bda1
phyco1991 <phyco1991@users.noreply.github.com>
2022-12-28 01:47:49 +0800

    Update PatreonVideoIncreasetoUsableScreenWidth.user.js
    Revised to actually work

--- PatreonVideoIncreasetoUsableScreenWidth.user.js
+++ PatreonVideoIncreasetoUsableScreenWidth.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Patreon Post Embedded Video Increase to Usable Screen Width
+// @name         Patreon Post Increase to Usable Screen Width
 // @namespace    https://github.com/phyco1991/randomuserscripts
-// @version      0.1
-// @description  Increase size of embedded Patreon videos to take up usable screen width
+// @version      0.2
+// @description  Increase size of Patreon posts to take up usable screen width
 // @author       Adam Fry
 // @match        https://www.patreon.com/posts/*
 // @require      https://raw.githubusercontent.com/greasemonkey/gm4-polyfill/master/gm4-polyfill.js
@@ -13,6 +13,5 @@
 // ==/UserScript==
 
 $(document).ready(function () {
-const elementclass = document.querySelector(".plJBe");
-    elementclass.setAttribute('style', 'max-width:none !important;');
+    $( "div" ).css( "max-width", "none" );
 });
